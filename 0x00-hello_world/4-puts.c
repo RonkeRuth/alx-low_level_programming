21e77065724b708b25fcdaa3af6bd3228222c752
ronke <ronkeruth091@gmail.com>
2023-03-16 21:55:13 -0700

    q0-Q4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+*main.Entry point
+*
+*Return:Always 0 (Success)
+*/
+in main(void)
+{
+	put("\"programmin is like building a multiple puzzle")
+	return (0):
+}
